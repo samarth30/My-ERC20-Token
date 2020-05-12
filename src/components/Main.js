554ffe9c880489a25b1d1ecb938c750cc072f252
samarth <samarth.gugnani30@gmail.com>
2020-05-12 21:25:58 +0530

    main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,67 +1,56 @@
-import React, { Component } from 'react'
-import BuyForm from './BuyForm'
-import SellForm from './SellForm'
+import React, { useState } from "react";
+import BuyForm from "./BuyForm";
+import SellForm from "./SellForm";
 
-class Main extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentForm: 'buy'
-    }
-  }
+const Main = ({ ethBalance, tokenBalance, buyTokens, sellTokens }) => {
+  const [currentForm, setcurrentForm] = useState("buy");
 
-  render() {
-    let content
-    if(this.state.currentForm === 'buy') {
-      content = <BuyForm
-        ethBalance={this.props.ethBalance}
-        tokenBalance={this.props.tokenBalance}
-        buyTokens={this.props.buyTokens}
+  let content;
+  if (currentForm === "buy") {
+    content = (
+      <BuyForm
+        ethBalance={ethBalance}
+        tokenBalance={tokenBalance}
+        buyTokens={buyTokens}
       />
-    } else {
-      content = <SellForm
-        ethBalance={this.props.ethBalance}
-        tokenBalance={this.props.tokenBalance}
-        sellTokens={this.props.sellTokens}
+    );
+  } else {
+    content = (
+      <SellForm
+        ethBalance={ethBalance}
+        tokenBalance={tokenBalance}
+        sellTokens={sellTokens}
       />
-    }
-
-    return (
-      <div id="content" className="mt-3">
-
-        <div className="d-flex justify-content-between mb-3">
-          <button
-              className="btn btn-light"
-              onClick={(event) => {
-                this.setState({ currentForm: 'buy' })
-              }}
-            >
-            Buy
-          </button>
-          <span className="text-muted">&lt; &nbsp; &gt;</span>
-          <button
-              className="btn btn-light"
-              onClick={(event) => {
-                this.setState({ currentForm: 'sell' })
-              }}
-            >
-            Sell
-          </button>
-        </div>
-
-        <div className="card mb-4" >
-
-          <div className="card-body">
-
-            {content}
-
-          </div>
-
-        </div>
-
-      </div>
     );
   }
-}
+
+  return (
+    <div id="content" className="mt-3">
+      <div className="d-flex justify-content-between mb-3">
+        <button
+          className="btn btn-light"
+          onClick={(event) => {
+            setcurrentForm("buy");
+          }}
+        >
+          Buy
+        </button>
+        <span className="text-muted">&lt; &nbsp; &gt;</span>
+        <button
+          className="btn btn-light"
+          onClick={(event) => {
+            setcurrentForm("sell");
+          }}
+        >
+          Sell
+        </button>
+      </div>
+
+      <div className="card mb-4">
+        <div className="card-body">{content}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
